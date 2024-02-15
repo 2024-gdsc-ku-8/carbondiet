642f1ab4bf10ad4c4c59a5add9321e9a50d01dc6
leestar0629 <yale901@naver.com>
2024-02-15 21:56:52 +0900

    login dto swagger 명시

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 export class LoginDto {
+  @IsString()
+  @ApiProperty({example:'korea123'})
   userid: string;
+  @IsString()
+  @ApiProperty({example:'password123@'})
   password: string;
 }
